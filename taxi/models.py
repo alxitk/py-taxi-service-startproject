3bf49d734610ea1b60368a27f26e8d0792a77e63
Oleksii Drenov <leshadrenev@gmail.com>
2025-06-22 17:27:50 +0300

    implement a str method

--- taxi/models.py
+++ taxi/models.py
@@ -6,6 +6,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
@@ -23,3 +26,6 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, blank=True)
+
+    def __str__(self):
+        return self.model
